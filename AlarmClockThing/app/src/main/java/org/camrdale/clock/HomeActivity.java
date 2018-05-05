4d29201eb9ec0c31a4b0e883f01e552e5539c1fc
camrdale <camrdale@gmail.com>
2018-05-05 02:22:22 -0700

    Add alarms for firing alarm, snooze, and sleep.

--- AlarmClockThing/app/src/main/java/org/camrdale/clock/HomeActivity.java
+++ AlarmClockThing/app/src/main/java/org/camrdale/clock/HomeActivity.java
@@ -1,7 +1,12 @@
 package org.camrdale.clock;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -19,6 +24,7 @@ import java.io.IOException;
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoField;
+import java.util.Calendar;
 import java.util.Locale;
 
 /**
@@ -26,24 +32,44 @@ import java.util.Locale;
  */
 public class HomeActivity extends Activity {
     private static final String TAG = HomeActivity.class.getSimpleName();
+    private static final String ALARM_ACTION = "org.camrdale.clock.ALARM";
+    private static final String EXPIRE_ALARM_ACTION = "org.camrdale.clock.EXPIRE_ALARM";
+    private static final String SNOOZE_ACTION = "org.camrdale.clock.SNOOZE";
+    private static final String SLEEP_ACTION = "org.camrdale.clock.SLEEP";
 
     private enum DisplayMode {
         HOUR_MINUTE,
         MINUTE_SECOND
     }
+    private enum AlarmState {
+        IDLE,
+        SLEEP,
+        FIRING,
+        SNOOZED
+    }
+
+    private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
 
     private AlphanumericDisplay mDisplay;
-    private DisplayMode mDisplayMode = DisplayMode.HOUR_MINUTE;
     private Gpio mRedLed;
     private Gpio mGreenLed;
     private Gpio mBlueLed;
     private ButtonInputDriver mButtonA;
     private ButtonInputDriver mButtonB;
     private ButtonInputDriver mButtonC;
+
     private CountDownTimer countDownTimer;
     private Clock clock;
-    private MediaPlayer mediaPlayer;
-    AudioManager audioManager;
+    private AlarmManager alarmManager;
+    private BroadcastReceiver alarmReceiver;
+    private PendingIntent nextAlarmIntent;
+    private PendingIntent snoozeIntent;
+    private PendingIntent sleepIntent;
+    private PendingIntent expireAlarmIntent;
+
+    private DisplayMode mDisplayMode = DisplayMode.HOUR_MINUTE;
+    private AlarmState mCurrentState = AlarmState.IDLE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +77,16 @@ public class HomeActivity extends Activity {
         setContentView(R.layout.activity_home);
 
         clock = Clock.systemDefaultZone();
+        alarmReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.i(TAG, "Alarm fired");
+                alarmReceived(intent);
+            }
+        };
+        registerReceiver(alarmReceiver, new IntentFilter(ALARM_ACTION));
+        registerReceiver(alarmReceiver, new IntentFilter(SNOOZE_ACTION));
+        registerReceiver(alarmReceiver, new IntentFilter(SLEEP_ACTION));
 
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         if (audioManager != null) {
@@ -58,6 +94,18 @@ public class HomeActivity extends Activity {
                     AudioManager.STREAM_MUSIC,
                     audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
                     0);
+        }
+
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        if (alarmManager != null) {
+            Log.i(TAG, "Scheduling alarm");
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+
+            nextAlarmIntent =
+                    PendingIntent.getBroadcast(this, 0, new Intent(ALARM_ACTION), 0);
+            alarmManager.set(AlarmManager.RTC_WAKEUP,
+                    calendar.getTimeInMillis() + 60 * 1000, nextAlarmIntent);
         }
 
         try {
@@ -107,7 +155,6 @@ public class HomeActivity extends Activity {
         }
 
         countDownTimer = new CountDownTimer(Long.MAX_VALUE, 1000) {
-
             // This is called after every 1 sec interval.
             public void onTick(long millisUntilFinished) {
                 showCurrentTime();
@@ -168,16 +215,40 @@ public class HomeActivity extends Activity {
             } catch (Exception e) {
                 Log.e(TAG, "Failed to light the LED.", e);
             }
-            try {
-                if (mediaPlayer != null) {
-                    if (mediaPlayer.isPlaying()) {
-                        mediaPlayer.stop();
+            switch (mCurrentState) {
+                case IDLE:
+                case SLEEP:
+                    if (sleepIntent != null) {
+                        sleepIntent.cancel();
+                        sleepIntent = null;
                     }
-                    mediaPlayer.release();
-                    mediaPlayer = null;
-                }
-            } catch (Exception e) {
-                Log.e(TAG, "Failed to stop the stream.", e);
+
+                    if (mCurrentState != AlarmState.SLEEP) {
+                        mCurrentState = AlarmState.SLEEP;
+                        startPlaying();
+                    }
+
+                    Log.i(TAG, "Scheduling expiry of sleep");
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTimeInMillis(System.currentTimeMillis());
+                    sleepIntent =
+                            PendingIntent.getBroadcast(this, 0, new Intent(SLEEP_ACTION), 0);
+                    alarmManager.set(AlarmManager.RTC_WAKEUP,
+                            calendar.getTimeInMillis() + 10 * 60 * 1000, sleepIntent);
+                    break;
+                case FIRING:
+                case SNOOZED:
+                    if (snoozeIntent != null) {
+                        snoozeIntent.cancel();
+                        snoozeIntent = null;
+                    }
+                    if (expireAlarmIntent != null) {
+                        expireAlarmIntent.cancel();
+                        expireAlarmIntent = null;
+                    }
+                    mCurrentState = AlarmState.IDLE;
+                    stopPlaying();
+                    break;
             }
             return true;
         }
@@ -187,28 +258,114 @@ public class HomeActivity extends Activity {
             } catch (Exception e) {
                 Log.e(TAG, "Failed to unlight the LED.", e);
             }
-            try {
-                if (mediaPlayer == null) {
-                    mediaPlayer = new MediaPlayer();
-                }
-                String url = "https://streams2.kqed.org/kqedradio"; // your URL here
-                mediaPlayer.reset();
-                mediaPlayer.setAudioAttributes(new AudioAttributes.Builder()
-                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                        .build());
-                mediaPlayer.setDataSource(url);
-                mediaPlayer.prepareAsync();
-                mediaPlayer.setOnPreparedListener(mp -> {
-                    Log.i(TAG, "Starting playback of media.");
-                    mp.start();
-                    mp.setVolume(1.0f, 1.0f);
-                });
-            } catch (Exception e) {
-                Log.e(TAG, "Failed to start the stream.", e);
+            switch (mCurrentState) {
+                case FIRING:
+                    mCurrentState = AlarmState.SNOOZED;
+                    stopPlaying();
+
+                    Log.i(TAG, "Scheduling expiry of snooze");
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTimeInMillis(System.currentTimeMillis());
+                    snoozeIntent =
+                            PendingIntent.getBroadcast(this, 0, new Intent(SNOOZE_ACTION), 0);
+                    alarmManager.set(AlarmManager.RTC_WAKEUP,
+                            calendar.getTimeInMillis() + 2 * 60 * 1000, snoozeIntent);
+                    break;
+                case SLEEP:
+                    if (sleepIntent != null) {
+                        sleepIntent.cancel();
+                        sleepIntent = null;
+                    }
+                    mCurrentState = AlarmState.IDLE;
+                    stopPlaying();
+                    break;
             }
             return true;
         }
         return super.onKeyUp(keyCode, event);
+    }
+
+    private void alarmReceived(Intent intent) {
+        Log.i(TAG, "Received alarm: " + intent.toString());
+        if (ALARM_ACTION.equals(intent.getAction()) && mCurrentState != AlarmState.FIRING) {
+            if (snoozeIntent != null) {
+                snoozeIntent.cancel();
+                snoozeIntent = null;
+            }
+            if (sleepIntent != null) {
+                sleepIntent.cancel();
+                sleepIntent = null;
+            }
+            if (expireAlarmIntent != null) {
+                expireAlarmIntent.cancel();
+                expireAlarmIntent = null;
+            }
+            mCurrentState = AlarmState.FIRING;
+            startPlaying();
+
+            Log.i(TAG, "Scheduling expiry of alarm");
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+            expireAlarmIntent =
+                    PendingIntent.getBroadcast(this, 0, new Intent(EXPIRE_ALARM_ACTION), 0);
+            alarmManager.set(AlarmManager.RTC_WAKEUP,
+                    calendar.getTimeInMillis() + 5 * 60 * 1000, expireAlarmIntent);
+
+            Log.i(TAG, "Scheduling next alarm");
+            nextAlarmIntent =
+                    PendingIntent.getBroadcast(this, 0, new Intent(ALARM_ACTION), 0);
+            alarmManager.set(AlarmManager.RTC_WAKEUP,
+                    calendar.getTimeInMillis() + 15 * 60 * 1000, nextAlarmIntent);
+        } else if (EXPIRE_ALARM_ACTION.equals(intent.getAction()) && mCurrentState == AlarmState.FIRING) {
+            if (snoozeIntent != null) {
+                snoozeIntent.cancel();
+                snoozeIntent = null;
+            }
+            mCurrentState = AlarmState.IDLE;
+            stopPlaying();
+        } else if (SNOOZE_ACTION.equals(intent.getAction()) && mCurrentState == AlarmState.SNOOZED) {
+            mCurrentState = AlarmState.FIRING;
+            startPlaying();
+        } else if (SLEEP_ACTION.equals(intent.getAction()) && mCurrentState == AlarmState.SLEEP) {
+            mCurrentState = AlarmState.IDLE;
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        try {
+            if (mediaPlayer == null) {
+                mediaPlayer = new MediaPlayer();
+            }
+            String url = "https://streams2.kqed.org/kqedradio"; // your URL here
+            mediaPlayer.reset();
+            mediaPlayer.setAudioAttributes(new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build());
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepareAsync();
+            mediaPlayer.setOnPreparedListener(mp -> {
+                Log.i(TAG, "Starting playback of media.");
+                mp.start();
+                mp.setVolume(1.0f, 1.0f);
+            });
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to start the stream.", e);
+        }
+    }
+
+    private void stopPlaying() {
+        try {
+            if (mediaPlayer != null) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                }
+                mediaPlayer.release();
+                mediaPlayer = null;
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to stop the stream.", e);
+        }
     }
 
     @Override
